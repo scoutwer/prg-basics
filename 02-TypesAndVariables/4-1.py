3c22c14c839d5619e0a6ab452bcf5426ef9ebbbb
Krzysztof Kurlit <kkurlit241@gmail.com>
2024-10-08 11:59:56 +0200

    sekcja 4 skończona

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,9 @@
+###
+# Displaying student's personal data
+#
+name = "Adam"
+age = 5
+height =  185
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age + 6 } years old.  ")
