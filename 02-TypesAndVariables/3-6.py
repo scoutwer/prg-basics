95ca23bb609aa6c0a3f42045c10fcbbc98dce707
Krzysztof Kurlit <kkurlit241@gmail.com>
2024-10-08 11:39:40 +0200

    sekcja 3 skończona

---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,16 @@
+import math 
+ 
+ # specifing height
+
+height_1 = 1.85
+height_2 = 20 
+
+
+# calculating distance from the horizon 
+
+distance_1 = math.sqrt(height_1) * 3.57
+distance_2 = math.sqrt(height_2) * 3.57
+
+
+print("distance from horizon is: ", distance_1)
+print("distance from horizon is: ", distance_2)
